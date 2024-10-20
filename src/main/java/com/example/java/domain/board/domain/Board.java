30812a790c29880a643aa5909828cb8bf4e1c64c
imhanseong <hans_hanseong@dsm.hs.kr>
2024-10-20 23:19:49 +0900

    add :: board entity 생성

---
+++ src/main/java/com/example/java/domain/board/domain/Board.java
@@ -0,0 +1,23 @@
+package com.example.java.domain.board.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private LocalDateTime localDateTime;
+    private String title;
+    private String content;
+}
