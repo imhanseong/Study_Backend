4b7ffc60b5719f586b4455c0d69097a234cbed32
imhanseong <hans_hanseong@dsm.hs.kr>
2024-10-21 09:03:17 +0900

    fix :: board request 수정

--- src/main/java/com/example/java/domain/board/presentation/dto/BoardRequest.java
+++ src/main/java/com/example/java/domain/board/presentation/dto/BoardRequest.java
@@ -1,7 +1,10 @@
 package com.example.java.domain.board.presentation.dto;
 
+import lombok.Getter;
 
+@Getter
 public class BoardRequest {
+    private String name;
     private String title;
     private String content;
 }
